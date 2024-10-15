053c3d0c0a3e0a12a7bc83b9cda424e3fc839a98
Orlando Flaschka <falschkaorlando@gmail.com>
2024-10-14 23:57:59 -0300

    commit final, con creacion de archivo

--- integrador.py
+++ integrador.py
@@ -11,15 +11,17 @@ def porcentaje(parte, total):
     
 
 
-def pre_procesamiento(datos_formateados):
+def pre_procesamiento(datos_formateados, parametros):
 
 #---------------------------------análisis por sexo----------------------------------
-    print("----------------------------ANÁLISIS POR SEXO----------------------------")
+    print("1----------------------------ANÁLISIS POR SEXO----------------------------")
 
     #calculo y formateo la variable "sexo" de cada registro
     for x in datos_formateados:
         #si está bien formateado no lo toco
-        if x["sexo"] != "M" and x["sexo"] != "F":
+        if x["sexo"] == "M" or x["sexo"] == "F":
+            x.update({"observaciones": "---"})
+        elif x["sexo"] != "M" and x["sexo"] != "F":
             #si se puede formatear considerando la primer letra lo hago
             x["sexo"] = str.upper(x["sexo"])
             if x["sexo"].startswith("F"):
@@ -49,7 +51,7 @@ def pre_procesamiento(datos_formateados):
 
 #-------------------------------------análisis por vacuna--------------------------------------
 
-    print("----------------------------ANÁLISIS POR TIPO DE VACUNA----------------------------")
+    print("2----------------------------ANÁLISIS POR TIPO DE VACUNA----------------------------")
 
     #creo un set para tomar todas las vacunas diferentes
     #sin un primer elemento me lo toma como un diccionario y se rompe
@@ -74,7 +76,7 @@ def pre_procesamiento(datos_formateados):
 
 
 #--------------------------------------análisis por jurisdicción---------------------------------------
-    print("----------------------------ANÁLISIS POR JURISDICCIÓN/PROVINCIA----------------------------")
+    print("3y4---------------------------ANÁLISIS POR JURISDICCIÓN/PROVINCIA----------------------------")
     jurisdiccionesdic={}
 
     for reg in datos_formateados:
@@ -86,11 +88,59 @@ def pre_procesamiento(datos_formateados):
             print(f"{sum((i["jurisdiccion_residencia_id"] == dis[1]) for i in datos_formateados)} dosis sin registro de jurisdicción")
 
         else:
-            print(f"{sum((i["jurisdiccion_residencia_id"] == dis[1]) for i in datos_formateados)} dosis en la provincia de {dis[0]}")
-            print(f"{sum((i["jurisdiccion_residencia_id"] == dis[1] and i["nombre_dosis_generica"] == "2da") for i in datos_formateados)}  fueron 2da dosis en la provincia de {dis[0]}")
-
-
+            #imprimo las dosis con provincias registradas
+            if dis[0] != "S.I.":
+                print(f"{sum((i["jurisdiccion_residencia_id"] == dis[1]) for i in datos_formateados)} dosis en la provincia de {dis[0]}")
+                print(f"{sum((i["jurisdiccion_residencia_id"] == dis[1] and i["nombre_dosis_generica"] == "2da") for i in datos_formateados)}  fueron 2da dosis en la provincia de {dis[0]}")
+            
+            #imprimo las dosis sin provincias registradas
+            else:
+                print(f"{sum((i["jurisdiccion_residencia_id"] == dis[1]) for i in datos_formateados)} dosis sin registro de provincia {dis[0]}")
+                print(f"{sum((i["jurisdiccion_residencia_id"] == dis[1] and i["nombre_dosis_generica"] == "2da") for i in datos_formateados)}  fueron 2da dosis sin registro de provincia")
   
+
+
+#--------------------------------------análisis por edad---------------------------------------
+    print("5---------------------------ANÁLISIS POR EDAD----------------------------")
+
+    mayores_edad=[]
+
+    for reg in datos_formateados:
+        if len(str.split(reg["grupo_etario"] , "-")) < 2:
+            if reg["grupo_etario"] == ">=100" and reg["nombre_dosis_generica"] == "Refuerzo":
+                mayores_edad.append(reg)          
+            continue
+        elif int(str.split(reg["grupo_etario"] , "-")[0]) < 60 :
+            continue
+        elif reg["nombre_dosis_generica"] == "Refuerzo" :
+            mayores_edad.append(reg)
+        else:
+            continue 
+
+
+    print(f"{len(mayores_edad)} mayores de 60 años que recibieron dosis de refuerzo")
+
+#--------------------------------------CREACION DEL CSV---------------------------------------
+    print("----------------------------CREACION DEL CSV----------------------------")
+
+
+    parametros.append("observaciones")
+
+    encabezado= (",".join(parametros))
+    
+
+    inconsistencias = open("inconsistencias.csv","w")
+
+    inconsistencias.write(encabezado+"\n")
+
+    for reg in datos_formateados:
+        if reg["observaciones"] != "---":
+            lista_params=[]
+            for par in parametros:
+                lista_params.append(reg.get(par,"---"))
+            registro_formateado= ",".join(lista_params).replace("\n","")
+            inconsistencias.write(registro_formateado+"\n")
+    inconsistencias.close()  
 
 
 
@@ -105,44 +155,53 @@ def cargarArchivos():
         datos_formateados = []
 
         for ix, lin in enumerate(muestra.readlines()):
-            
+            lin.replace("\n", "")            
             #cargo los encabezados para definirlos como claves de un arreglo de diccionarios
-            if ix == 0:
-                parametros = lin.split(",")
 
-                #elimino el \n que define el salto de línea del último encabezado
-                parametros[-1]  = lin.split(",")[-1][:-1]
-                      
+            #si es la primer fila, lo tomo como encabezado
+            if ix == 0:
+                parametros = lin.replace("\n", "").split(",")
+            #sino, lo cargo como un registro                     
             else:
                 
-                #verifico que los registros no tengan comas que hayan roto la estructura de las celdas
+                #verifico que los registros tengan la misma cantidad de propiedades que el encabezado
                 if len(lin.split(",")) != len(parametros):
-                    print("----garca-----")
+                    print("----roto-----")
                     #falta
                     #       el 
                     #           código
                     #               de tratamiento
                     #                   de ésta cosa
                     print(lin)
-                    print("---fingarca---")
+                    print("---finroto---")
+                    # print("intento arreglar")
+                    # lin.replace('"','')
+                    # lin.replace("'", "")
+
+                    # if len(lin.replace("'","").split(",")) != len(parametros):
+                    #     print("no funcionó")
+                    #     print(lin)
+
+                    # else:
+                    #     print("funcionó")
+                    #     print(lin)
+                    # quit()
+                    #NUNCA FUNCIONÓ LO QUE INTENÉ ARREGLAR
+
                 else:
                 
-                    #creo un diccionario usando los encabezados como claves y las filas como valores
+                    #creo un diccionario usando los encabezados como claves y celdas de las filas
                     registro = {}
 
                     for ix, par in enumerate(parametros):
-                            registro.update({par: lin.split(",")[ix]})
-                    
-                    #elimino el \n, que define el salto de línea, del último elemento de la fila 
-                    registro.update({parametros[-1]:registro[parametros[-1]][:-1]})
+                            registro.update({par: lin.split(",")[ix]})                  
+
 
                     #agrego el nuevo diccionario a la lista
                     datos_formateados.append(registro)
  
 
-        pre_procesamiento(datos_formateados) 
-
-
+        pre_procesamiento(datos_formateados, parametros) 
     
 
 
---
+++ readme.md
@@ -0,0 +1,31 @@
+Desafío Integrador - 1er Parte
+
+Alumno:
+Nombre completo: Flaschka Orlando René
+DNI: 37046298
+email: flaschkaorlando@gmail.com
+
+Conjunto utilizado: Conjunto de datos 1
+
+Análisis Solicitado
+El informe debe incluir lo siguiente:
+Estadísticas Descriptivas
+
+1. Distribución por Género:
+   ○ Calcular cuántas personas de cada género (sexo) han recibido la vacuna.
+2. Vacunas Aplicadas por Tipo de Vacuna:
+   ○ Determinar cuántas personas han recibido cada tipo de vacuna (vacuna) y
+   calcular la proporción respecto al total.
+   Agrupación y Conteo
+3. Dosis por Jurisdicción de Residencia:
+   ○ Agrupar los datos por jurisdiccion_residencia y contar cuántas
+   personas han sido vacunadas en cada provincia.
+   Pedido Especial
+   El Ministro ha solicitado información específica sobre:
+4. Cuántas personas han recibido la segunda dosis en cada jurisdicción.
+5. Cuántas personas mayores de 60 años han recibido dosis de refuerzo.
+
+Resúmen:
+Todas las consignas fueron completadas casi por completo, el único inconveniente lo encontré al intentar formatear un registro que estaba corrupto (entiendo que una comilla o coma de más pero no llegué a encontrarle la vuelta) y al no poder formatearlo, lo descarté de la muestra final.
+
+                                                                                                                                                ...it ain't much, but it's honest work
